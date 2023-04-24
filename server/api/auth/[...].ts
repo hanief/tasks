7df73c23ffd7619085fc1d97be8c41021aef8046
Hanief <hanief@gmail.com>
2023-04-24 17:49:14 +0700

    try another way

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -2,6 +2,13 @@ import { NuxtAuthHandler } from '#auth'
 import Auth0Provider from 'next-auth/providers/auth0'
 
 export default NuxtAuthHandler({
+  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    session: async ({ session, token }) => {
+      session.user_id = token.sub
+      return Promise.resolve(session)
+    },
+  },
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     Auth0Provider.default({
